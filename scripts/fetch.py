1773011ea8e53ac78ef1c9aad73ec223a9215d83
Mauricio Foronda <josemauricioforonda@gmail.com>
2020-10-20 16:04:24 -0400

    cambiar la fuente de excel a csv (oep ha dejado de actualizar el excel)

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -2,6 +2,7 @@
 
 import requests
 import pandas as pd
+import csv
 
 headers = {
     'authority': 'computo.oep.org.bo',
@@ -20,10 +21,20 @@ headers = {
     'accept-language': 'en-US,en;q=0.9,es;q=0.8',
 }
 
-data = '{"tipoArchivo":"EXCEL"}'
+data = '{"tipoArchivo":"CSV"}'
 
-response = requests.post('https://computo.oep.org.bo/api/v1/descargar', headers=headers, data=data)
-excel = requests.get(response.json()['datoAdicional']['archivo']).content
-df = pd.concat([pd.read_excel(excel, sheet_name=i) for i in range(2)])
-filename = 'datos/{}.csv'.format('_'.join(response.json()['datoAdicional']['archivo'].split('/')[-1].split('_')[1:3]))
+meta = requests.post('https://computo.oep.org.bo/api/v1/descargar', headers=headers, data=data)
+response = requests.get(meta.json()['datoAdicional']['archivo'])
+csvdata = csv.reader(response.content.decode('iso8859').splitlines(), delimiter=',')
+
+data = []
+for row in csvdata:
+    if len(row) == 40:
+        data.append(row)
+    else:
+        data.append(row[:15] + [ ''.join(row[15:16+(len(row)-40)]) ] + row[16+(len(row)-40):])
+
+df = pd.DataFrame(data[1:], columns=data[0])
+df['RECINTO'] = df.RECINTO.str.replace('[`"\']', '')
+filename = 'datos/{}.csv'.format('_'.join(meta.json()['datoAdicional']['archivo'].split('/')[-1].split('_')[1:3]))
 df.to_csv(filename, index=False, encoding='utf-8')
